cc4b3003136b00fc260fa8c4bbf62892ec9477cd
Vikas Mishra <vikmishr3@publicisgroupe.net>
2022-11-17 01:40:06 +0530

    second commit with all the java file

---
+++ src/test/java/regression/Testcase2.java
@@ -0,0 +1,48 @@
+package regression;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+
+public class Testcase2 {
+	
+	@Test
+	public void validateTitles() {
+		
+		System.out.println("Beginning");
+		
+		String expected_title = "Yahoo.com"; //excel
+		String actual_title = "Gmail.com"; //selenium
+		
+		/*if(expected_title.equals(actual_title)) {
+			
+			System.out.println("Test case pass");
+		}else {
+			
+			System.out.println("Test case fail");
+		}*/
+		
+		SoftAssert softAssert = new SoftAssert();
+		
+		System.out.println("Validating title");
+		
+		
+		
+		softAssert.assertEquals(actual_title, expected_title);//-----fail
+		//isElementPresent("xpath") - True, False
+		//Assert.assertTrue(false,"Element not found");
+		System.out.println("Validating image");
+		softAssert.assertEquals(true, false,"image not present"); //---- fail
+		
+		System.out.println("validate text box presence");
+		softAssert.assertEquals(true, false,"text box not present"); //-- fail
+		//Assert.fail("Failing the test as the condition is not met");
+		System.out.println("Ending");
+	
+		
+		softAssert.assertAll();
+		
+	}
+
+}
---
+++ src/test/java/testcases/TestFailure.java
@@ -0,0 +1,16 @@
+package testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestFailure {
+	
+	@Test
+	public void doLogin() {
+		
+		Assert.fail("Failing the login test");
+		System.out.println("Capture Screenshot");
+	}
+
+
+}
